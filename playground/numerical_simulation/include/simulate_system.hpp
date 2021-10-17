d37284f955e0cca8d5b6cc7cad5d6bab41037e8b
Rodrigo Duran <Rads2995@gmail.com>
2021-10-17 01:26:46 -0500

    Modified file to look better

--- playground/numerical_simulation/include/simulate_system.hpp
+++ playground/numerical_simulation/include/simulate_system.hpp
@@ -3,11 +3,8 @@
 
 #include <iostream>
 #include <fstream>
-#include <boost/array.hpp>
 
 #include <boost/numeric/odeint.hpp>
-
-using namespace boost::numeric::odeint;
 
 const double sigma = 10.0;
 const double R = 28.0;
@@ -15,17 +12,14 @@ const double b = 8.0 / 3.0;
 
 std::ofstream data("simulation.txt");
 
-
-typedef boost::array <double, 3> state_type;
-
-void lorenz (const state_type &x, state_type &dxdt, double t) {
+void lorenz (const std::array <double, 3> &x, std::array <double, 3> &dxdt, double t) {
 
     dxdt[0] = sigma * (x[1] - x[0]);
     dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
     dxdt[2] = -b * x[2] + x[0] * x[1];
 }
 
-void write_lorenz (const state_type &x, const double t) {
+void write_lorenz (const std::array <double, 3> &x, const double t) {
     
     data << t << '\t' << x[0] << '\t' << x[1] << '\t' << x[2] << std::endl;
 }
