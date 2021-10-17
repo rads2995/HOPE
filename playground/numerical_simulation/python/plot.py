6bfb37de59055df0b7e2888b7c70ff546f2ab74e
Rodrigo Duran <Rads2995@gmail.com>
2021-10-17 01:37:06 -0500

    Simplified simulation file and added python plot script for .txt results

--- playground/numerical_simulation/python/plot.py
+++ playground/numerical_simulation/python/plot.py
@@ -5,8 +5,8 @@ data = np.loadtxt("simulation.txt")
 
 time = data[:,0]
 x = data[:,1]
-xd = data[:,2]
-xdd = data[:,3]
+y = data[:,2]
+z = data[:,3]
 
-plt.plot(x,xd, 'r--')
-plt.savefig("plot.png")
+plt.plot(x, z, 'r--')
+plt.savefig("output.png")
--- playground/numerical_simulation/src/main.cpp
+++ playground/numerical_simulation/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     
-    state_type x = {10.0, 1.0, 1.0};
-    integrate (lorenz, x, 0.0, 25.0, 0.1, write_lorenz);
+    std::array <double, 3> x = {10.0, 1.0, 1.0};
+    boost::numeric::odeint::integrate (lorenz, x, 0.0, 25.0, 0.1, write_lorenz);
     return 0;
 }
