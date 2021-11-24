cb13fbe174ec2c384ad7952fb921c35ac4ae3d16
Rodrigo Duran <Rads2995@gmail.com>
2021-11-24 01:09:42 -0500

    Last commit, everything working

--- compile.sh
+++ compile.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
 clear
-echo "Compile simulation project..."
+echo "Starting dynamical system simulation..."
 echo "Creating build directory..."
 mkdir -p build
 echo "Generating CMake files for build directory..."
 cmake -S . -B build/
 echo "Switching to build directory..."
 cd build
-echo "Compiling project using Makefile..."
+echo "Compiling project using generated Makefile..."
 make
 echo "Running executable..."
 ./HOPE
-echo "Copy results into Python directory"
+echo "Copying results from simulation into Python directory..."
 cp simulation_result.txt ../python/
+echo "Switching to Python directory"
+cd ../python/
+echo "Running python script"
+python plot.py
+echo "Plot of resulting simulation has been generated..."
+echo "Simulation completed!"
