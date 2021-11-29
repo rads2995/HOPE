1f4cc8dd8f74ba9384ce4f181ce0ec2e285ee0af
Rodrigo Duran <Rads2995@gmail.com>
2021-11-28 20:05:18 -0500

    Testing for resizing matrices

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,7 @@
 std::ofstream data("simulation_result.txt");
 
 // Define {x} vector size and numerical integration parameters
-constexpr unsigned int num_states = 2;  // number of states
-constexpr PRECISION t0 = 0;             // initial time (s)
-constexpr PRECISION tf = 50;            // final time (s)
-constexpr PRECISION step_size = 0.01;   // step size (s)
+constexpr unsigned int num_states = 0;  // number of states
 
 // All matrices and vectors are initialized to zero
 Eigen::Matrix<PRECISION, num_states, num_states> A = Eigen::Matrix<PRECISION, num_states, num_states>::Zero();
@@ -61,8 +58,6 @@ int main()
    
     system.matrix_resize();
     
-    std::cout << system.get_m_A() << std::endl;
-
     // Copy object's matrices back to the global matrices
     A = system.get_m_A();
     B = system.get_m_B();
@@ -71,8 +66,10 @@ int main()
     u = system.get_m_u();
     x0 = system.get_m_x0();
    
-    std::cout << A << std::endl;
-
+    PRECISION t0 = 0;             // initial time (s)
+    PRECISION tf = 50;            // final time (s)
+    PRECISION step_size = 0.01;   // step size (s)
+    
     // Call stepper function (Runge-Kutta Dormand-Prince 5 method)
     runge_kutta_dopri5<Eigen::Matrix<PRECISION, num_states, 1>, PRECISION, Eigen::Matrix<PRECISION, num_states, 1>, PRECISION, vector_space_algebra> stepper;
     // Call integrator function (equidistant integration)
